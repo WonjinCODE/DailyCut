763f42d4220ce6a2b94449c8fad73520c9a290b1
WonjinCODE <kimwonjin08@gmail.com>
2026-04-16 22:52:21 +0900

    마이페이지 구성 50% 완료(UI는 미완)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import ResultPage from './pages/ResultPage';
 import BingeCalcPage from './pages/BingeCalcPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+// 👇 1. MyPage 컴포넌트를 불러옵니다!
+import MyPage from './pages/MyPage'; 
 
 // 임시 컴포넌트
 const NotFoundPage = () => <div className="p-20 text-center text-4xl font-bold text-white bg-dark min-h-screen">404 - 페이지를 찾을 수 없습니다.</div>;
@@ -20,6 +22,9 @@ function App() {
         {/* ⭐ 로그인 및 회원가입 라우트 추가 */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        
+        {/* 👇 2. 마이페이지 라우트를 추가합니다! (반드시 * 라우트보다 위에 있어야 해요) */}
+        <Route path="/mypage" element={<MyPage />} />
         
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
